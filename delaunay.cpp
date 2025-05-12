4ad4e2464d539747c7d4b4b3a789aa2b17328d44
orzzzjq <orzzzjq@gmail.com>
2025-05-12 16:11:23 +0800

    combine the interfaces for 2D and 3D Delaunay & Euclidean MST, starting with periodic sets

--- delaunay.cpp
+++ delaunay.cpp
@@ -11,11 +11,9 @@
 
 #include <boost/graph/kruskal_min_spanning_tree.hpp>
 
-#include <vector>
+#include <Eigen/Dense>
 
-#include <pybind11/pybind11.h>
-#include <pybind11/numpy.h>
-#include <pybind11/stl.h>
+#include <vector>
 
 using K2 = CGAL::Epeck_d< CGAL::Dimension_tag<2> >;
 using Point2 = K2::Point_d;
@@ -37,26 +35,39 @@ using EdgeIterator = boost::graph_traits<Graph>::edge_iterator;
 
 using namespace std;
 
-namespace py = pybind11;
-
-py::array_t<int> delaunay2d(py::array_t<double> points) {
-    auto buf = points.request();
-    if (buf.ndim != 2 || buf.shape[1] != 2) {
-        throw std::invalid_argument("Input must be a 2D NumPy array of shape (n, 2)");
+Eigen::MatrixXi DelaunaySkeleton(const Eigen::MatrixXd& points) {
+    if (points.rows() != 2 && points.rows() != 3) {
+        throw std::invalid_argument("Input must be a 2D NumPy array of shape (2, n) or (3, n)");
     }
 
-    size_t n = buf.shape[0];
-    auto ptr = static_cast<double*>(buf.ptr);
-
-    std::vector<Point2> p;
-    for (int i = 0; i < n; ++i) {
-        p.push_back(Point2(ptr[i * 2], ptr[i * 2 + 1]));
-    }
-
-    Gudhi::alpha_complex::Alpha_complex<K2> alphaComplex(p);
+    int n = points.cols(), d = points.rows();
 
     Gudhi::Simplex_tree complex;
-    alphaComplex.create_complex(complex, INFINITY, false, true);
+
+    if (d == 2) {
+        vector<double> coord(2);
+        vector<Point2> p;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < d; ++j) {
+                coord[j] = points(j, i);
+            }
+            p.push_back(Point2(coord[0], coord[1]));
+        }
+        Gudhi::alpha_complex::Alpha_complex<K2> alphaComplex(p);
+        alphaComplex.create_complex(complex, INFINITY, false, true);
+    }
+    else {
+        vector<double> coord(3);
+        vector<Point3> p;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < d; ++j) {
+                coord[j] = points(j, i);
+            }
+            p.push_back(Point3(coord[0], coord[1], coord[2]));
+        }
+        Gudhi::alpha_complex::Alpha_complex<K3> alphaComplex(p);
+        alphaComplex.create_complex(complex, INFINITY, false, true);
+    }
 
     vector<vector<int>> edges;
 
@@ -70,40 +81,56 @@ py::array_t<int> delaunay2d(py::array_t<double> points) {
         }
     }
 
-    // Convert to a 2D NumPy array
-    py::ssize_t dims[2] = {static_cast<py::ssize_t>(edges.size()), 2};
-    py::array_t<int> result(dims);
-    auto result_buf = result.mutable_unchecked<2>();
+    Eigen::MatrixXi result(size(edges), 2);
 
     for (size_t i = 0; i < size(edges); ++i) {
         for (size_t j = 0; j < 2; ++j) {
-            result_buf(i, j) = edges[i][j];
+            result(i, j) = edges[i][j];
         }
     }
 
     return result;
 }
 
-py::array_t<int> delaunay3d(py::array_t<double> points) {
-    auto buf = points.request();
-    if (buf.ndim != 2 || buf.shape[1] != 3) {
-        throw std::invalid_argument("Input must be a 2D NumPy array of shape (n, 3)");
+Eigen::MatrixXi EuclideanMST(const Eigen::MatrixXd& points) {
+    if (points.rows() != 2 && points.rows() != 3) {
+        throw std::invalid_argument("Input must be a 2D NumPy array of shape (2, n) or (3, n)");
     }
 
-    size_t n = buf.shape[0];
-    auto ptr = static_cast<double*>(buf.ptr);
-
-    std::vector<Point3> p;
-    for (int i = 0; i < n; ++i) {
-        p.push_back(Point3(ptr[i * 3], ptr[i * 3 + 1], ptr[i * 3 + 2]));
-    }
-
-    Gudhi::alpha_complex::Alpha_complex<K3> alphaComplex(p);
+    int n = points.cols(), d = points.rows();
 
     Gudhi::Simplex_tree complex;
-    alphaComplex.create_complex(complex, INFINITY, false, true);
 
-    vector<vector<int>> edges;
+    if (d == 2) {
+        vector<double> coord(2);
+        vector<Point2> p;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < d; ++j) {
+                coord[j] = points(j, i);
+            }
+            p.push_back(Point2(coord[0], coord[1]));
+        }
+        Gudhi::alpha_complex::Alpha_complex<K2> alphaComplex(p);
+        alphaComplex.create_complex(complex, INFINITY, false, true);
+    }
+    else {
+        vector<double> coord(3);
+        vector<Point3> p;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < d; ++j) {
+                coord[j] = points(j, i);
+            }
+            p.push_back(Point3(coord[0], coord[1], coord[2]));
+        }
+        Gudhi::alpha_complex::Alpha_complex<K3> alphaComplex(p);
+        alphaComplex.create_complex(complex, INFINITY, false, true);
+    }
+
+    Graph graph;
+
+    for (int i = 0; i < n; ++i) {
+        add_vertex(i, graph);
+    }
 
     for (auto simplex : complex.skeleton_simplex_range(1)) {
         if (complex.dimension(simplex)) {
@@ -111,72 +138,20 @@ py::array_t<int> delaunay3d(py::array_t<double> points) {
             for (auto v : complex.simplex_vertex_range(simplex)) {
                 id.push_back(int(v));
             }
-            edges.push_back(id);
-        }
-    }
-
-    py::ssize_t dims[2] = {static_cast<py::ssize_t>(edges.size()), 2};
-    py::array_t<int> result(dims);
-    auto result_buf = result.mutable_unchecked<2>();
-
-    for (size_t i = 0; i < size(edges); ++i) {
-        for (size_t j = 0; j < 2; ++j) {
-            result_buf(i, j) = edges[i][j];
-        }
-    }
-
-    return result;
-}
-
-py::array_t<int> emst2d(py::array_t<double> points) {
-    auto buf = points.request();
-    if (buf.ndim != 2 || buf.shape[1] != 2) {
-        throw std::invalid_argument("Input must be a 2D NumPy array of shape (n, 2)");
-    }
-
-    size_t n = buf.shape[0];
-    auto ptr = static_cast<double*>(buf.ptr);
-
-    std::vector<Point2> p;
-    for (int i = 0; i < n; ++i) {
-        p.push_back(Point2(ptr[i * 2], ptr[i * 2 + 1]));
-    }
-
-    Gudhi::alpha_complex::Alpha_complex<K2> alphaComplex(p);
-
-    Gudhi::Simplex_tree complex;
-    alphaComplex.create_complex(complex, INFINITY, false, true);
-
-    for (auto simplex : complex.skeleton_simplex_range(1)) {
-        if (complex.dimension(simplex)) {
-            vector<int> id;
-            for (auto v : complex.simplex_vertex_range(simplex)) {
-                id.push_back(int(v));
+            int s = id[0], t = id[1];
+            double sq_dist = 0;
+            if (d == 2) {
+                double dx = points(0, s) - points(0, t), dy = points(1, s) - points(1, t);
+                sq_dist = dx * dx + dy * dy;
             }
-            auto dx = p[id[0]][0] - p[id[1]][0], dy = p[id[0]][1] - p[id[1]][1];
-            auto sq_dist = (dx * dx + dy * dy);
-            complex.assign_filtration(simplex, to_double(sq_dist));
-        }
-        else {
-            complex.assign_filtration(simplex, 0);
+            else {
+                double dx = points(0, s) - points(0, t), dy = points(1, s) - points(1, t), dz = points(2, s) - points(2, t);
+                sq_dist = dx * dx + dy * dy + dz * dz;
+            }
+            add_edge(s, t, sq_dist, graph);
         }
     }
     
-    Graph graph;
-
-    for (int i = 0; i < size(p); ++i) {
-        add_vertex(i, graph);
-    }
-    for (auto simplex : complex.skeleton_simplex_range(1)) {
-        if (complex.dimension(simplex)) {
-            vector<int> id;
-            for (auto v : complex.simplex_vertex_range(simplex)) {
-                id.push_back(int(v));
-            }
-            add_edge(id[0], id[1], complex.filtration(simplex), graph);
-        }
-    }
-
     std::list<EdgeDescriptor> mst;
     boost::kruskal_minimum_spanning_tree(graph, std::back_inserter(mst));
 
@@ -189,304 +164,68 @@ py::array_t<int> emst2d(py::array_t<double> points) {
        edges.push_back(vector<int>{static_cast<int>(s), static_cast<int>(t)});
     }
 
-    // Convert to a 2D NumPy array
-    py::ssize_t dims[2] = {static_cast<py::ssize_t>(edges.size()), 2};
-    py::array_t<int> result(dims);
-    auto result_buf = result.mutable_unchecked<2>();
+    Eigen::MatrixXi result(size(edges), 2);
 
     for (size_t i = 0; i < size(edges); ++i) {
         for (size_t j = 0; j < 2; ++j) {
-            result_buf(i, j) = edges[i][j];
+            result(i, j) = edges[i][j];
         }
     }
 
     return result;
 }
 
-py::array_t<int> emst3d(py::array_t<double> points) {
-    auto buf = points.request();
-    if (buf.ndim != 2 || buf.shape[1] != 3) {
-        throw std::invalid_argument("Input must be a 3D NumPy array of shape (n, 2)");
+double LatticeDiameter(const Eigen::MatrixXd& U) {
+    if (U.rows() != U.cols() || (U.cols() != 2 && U.cols() != 3)) {
+        throw std::invalid_argument("Input must be a 2x2 or 3x3 matrix");
     }
 
-    size_t n = buf.shape[0];
-    auto ptr = static_cast<double*>(buf.ptr);
-
-    std::vector<Point3> p;
-    for (int i = 0; i < n; ++i) {
-        p.push_back(Point3(ptr[i * 3], ptr[i * 3 + 1], ptr[i * 3 + 2]));
-    }
-
-    Gudhi::alpha_complex::Alpha_complex<K3> alphaComplex(p);
-
-    Gudhi::Simplex_tree complex;
-    alphaComplex.create_complex(complex, INFINITY, false, true);
-
-    for (auto simplex : complex.skeleton_simplex_range(1)) {
-        if (complex.dimension(simplex)) {
-            vector<int> id;
-            for (auto v : complex.simplex_vertex_range(simplex)) {
-                id.push_back(int(v));
+    int d = U.rows();
+    int mask = 0;
+    double max_dist = 0, dist;
+    for (; mask < (1 << d); ++mask) {
+        Eigen::VectorXd v = Eigen::VectorXd::Zero(d);
+        for (int i = 0; i < d; ++i) {
+            if (mask & (1 << i)) { // add
+                v += U.col(i);
             }
-            auto dx = p[id[0]][0] - p[id[1]][0], dy = p[id[0]][1] - p[id[1]][1], dz = p[id[0]][2] - p[id[1]][2];
-            auto sq_dist = (dx * dx + dy * dy + dz * dz);
-            complex.assign_filtration(simplex, to_double(sq_dist));
+            else { // minus
+                v -= U.col(i);
+            }
         }
-        else {
-            complex.assign_filtration(simplex, 0);
-        }
+        dist = v.norm();
+        if (dist > max_dist) max_dist = dist;
     }
     
-    Graph graph;
-
-    for (int i = 0; i < size(p); ++i) {
-        add_vertex(i, graph);
-    }
-    for (auto simplex : complex.skeleton_simplex_range(1)) {
-        if (complex.dimension(simplex)) {
-            vector<int> id;
-            for (auto v : complex.simplex_vertex_range(simplex)) {
-                id.push_back(int(v));
-            }
-            add_edge(id[0], id[1], complex.filtration(simplex), graph);
-        }
-    }
-
-    std::list<EdgeDescriptor> mst;
-    boost::kruskal_minimum_spanning_tree(graph, std::back_inserter(mst));
-
-    vector<vector<int>> edges;
-
-    for (EdgeDescriptor e : mst)
-    {
-       VertexDescriptor s = source(e, graph);
-       VertexDescriptor t = target(e, graph);
-       edges.push_back(vector<int>{static_cast<int>(s), static_cast<int>(t)});
-    }
-
-    // Convert to a 2D NumPy array
-    py::ssize_t dims[2] = {static_cast<py::ssize_t>(edges.size()), 2};
-    py::array_t<int> result(dims);
-    auto result_buf = result.mutable_unchecked<2>();
-
-    for (size_t i = 0; i < size(edges); ++i) {
-        for (size_t j = 0; j < 2; ++j) {
-            result_buf(i, j) = edges[i][j];
-        }
-    }
-
-    return result;
+    return max_dist;
 }
 
-PYBIND11_MODULE(periodica, m) {
-    m.def("delaunay2d", &delaunay2d, "Compute 2D Delaunay triangulation",
-          py::arg("points"));
-    m.def("delaunay3d", &delaunay3d, "Compute 3D Delaunay triangulation",
-          py::arg("points"));
-    m.def("emst2d", &emst2d, "Compute 2D Euclidean minimum spanning tree",
-          py::arg("points"));
-    m.def("emst3d", &emst3d, "Compute 3D Euclidean minimum spanning tree",
-          py::arg("points"));
-}
+void testLatticeDiameter() {
+    Eigen::MatrixXd U(2, 2);
 
-void testGudhiAlpha2D() {
-    int n = 10000;
-    std::vector<Point2> points;
+    U(0, 0) = 1, U(0, 1) = 0;
+    U(1, 0) = 0, U(1, 1) = 1;
 
-    // ---------- generate random points ----------
-    CGAL::Random rd;
-    for (int i = 0; i < n; ++i) {
-        points.push_back(Point2(rd.get_double(), rd.get_double()));
-    }
-
-    // ---------- compute delaunay ----------
-    recordStart();
-
-    Gudhi::alpha_complex::Alpha_complex<K2> alphaComplex(points);
-
-    recordStop("2D Delaunay triangulation");
-
-    // ---------- create simplical complex ----------
-    recordStart();
-
-    Gudhi::Simplex_tree complex;
-    alphaComplex.create_complex(complex, INFINITY, false, true);
-
-    recordStop("Create complex");
-
-    // ---------- compute filtration values ----------
-    recordStart();
-
-    for (auto simplex : complex.skeleton_simplex_range(1)) {
-        if (complex.dimension(simplex)) {
-            vector<int> id;
-            for (auto v : complex.simplex_vertex_range(simplex)) {
-                id.push_back(int(v));
-            }
-            auto dx = points[id[0]][0] - points[id[1]][0], dy = points[id[0]][1] - points[id[1]][1];
-            auto sq_dist = (dx * dx + dy * dy);
-            complex.assign_filtration(simplex, to_double(sq_dist));
-        }
-        else {
-            complex.assign_filtration(simplex, 0);
-        }
-    }
-
-    recordStop("Assign weights");
-
-    // ---------- construct weighted graph ----------
-    recordStart();
-
-    Graph graph;
-
-    for (int i = 0; i < size(points); ++i) {
-        add_vertex(i, graph);
-    }
-    for (auto simplex : complex.skeleton_simplex_range(1)) {
-        if (complex.dimension(simplex)) {
-            vector<int> id;
-            for (auto v : complex.simplex_vertex_range(simplex)) {
-                id.push_back(int(v));
-            }
-            add_edge(id[0], id[1], complex.filtration(simplex), graph);
-        }
-    }
-
-    recordStop("2D Graph");
-
-    // ---------- compute minimum spanning tree ----------
-    recordStart();
-
-    std::list<EdgeDescriptor> mst;
-    boost::kruskal_minimum_spanning_tree(graph, std::back_inserter(mst));
-
-    recordStop("Minimum spanning tree");
-
-    // ---------- debugging ----------
-    
-    //cout << "Number of 1D faces in complex: " << complex.num_simplices_by_dimension()[1] << "\n";
-    //cout << "Number of edges in graph: " << num_edges(graph) << "\n";
-
-    //cout << "Graph edges:\n";
-    //for (auto simplex : complex.skeleton_simplex_range(1)) {
-    //    if (complex.dimension(simplex) == 0) continue;
-    //    cout << "\t";
-    //    for (auto v : complex.simplex_vertex_range(simplex)) {
-    //        cout << v << " ";
-    //    }
-    //    cout << ": " << complex.filtration(simplex) << "\n";
-    //}
-
-    //cout << "Number of edges in MST: " << mst.size() << "\n";
-    //cout << "MST edges:\n";
-    //for (EdgeDescriptor ed : mst)
-    //{
-    //    VertexDescriptor s = source(ed, graph);
-    //    VertexDescriptor t = target(ed, graph);
-    //    std::cout << "\t" << s << "--" << t << "\n";
-    //}
-}
-
-void testGudhiAlpha3D() {
-    int n = 100000;
-    std::vector<Point3> points;
-
-    // ---------- generate random points ----------
-    CGAL::Random rd;
-    for (int i = 0; i < n; ++i) {
-        points.push_back(Point3(rd.get_double(), rd.get_double(), rd.get_double()));
-    }
-
-    // ---------- compute delaunay ----------
-    recordStart();
-
-    Gudhi::alpha_complex::Alpha_complex<K3> alphaComplex(points);
-
-    recordStop("3D Delaunay triangulation");
-
-    // ---------- create simplical complex ----------
-    recordStart();
-
-    Gudhi::Simplex_tree complex;
-    alphaComplex.create_complex(complex, INFINITY, false, true);
-
-    recordStop("Create complex");
-
-    // ---------- compute filtration values ----------
-    recordStart();
-
-    for (auto simplex : complex.skeleton_simplex_range(1)) {
-        if (complex.dimension(simplex)) {
-            vector<int> id;
-            for (auto v : complex.simplex_vertex_range(simplex)) {
-                id.push_back(int(v));
-            }
-            auto dx = points[id[0]][0] - points[id[1]][0], dy = points[id[0]][1] - points[id[1]][1];
-            auto sq_dist = (dx * dx + dy * dy);
-            complex.assign_filtration(simplex, to_double(sq_dist));
-        }
-        else {
-            complex.assign_filtration(simplex, 0);
-        }
-    }
-
-    recordStop("Assign weights");
-
-    // ---------- construct weighted graph ----------
-    recordStart();
-
-    Graph graph;
-
-    for (int i = 0; i < size(points); ++i) {
-        add_vertex(i, graph);
-    }
-    for (auto simplex : complex.skeleton_simplex_range(1)) {
-        if (complex.dimension(simplex)) {
-            vector<int> id;
-            for (auto v : complex.simplex_vertex_range(simplex)) {
-                id.push_back(int(v));
-            }
-            add_edge(id[0], id[1], complex.filtration(simplex), graph);
-        }
-    }
-
-    recordStop("3D Graph");
-
-    // ---------- compute minimum spanning tree ----------
-    recordStart();
-
-    std::list<EdgeDescriptor> mst;
-    boost::kruskal_minimum_spanning_tree(graph, std::back_inserter(mst));
-
-    recordStop("Minimum spanning tree");
-
-    // ---------- debugging ----------
-
-    //cout << "Number of 1D faces in complex: " << complex.num_simplices_by_dimension()[1] << "\n";
-    //cout << "Number of edges in graph: " << num_edges(graph) << "\n";
-
-    //cout << "Graph edges:\n";
-    //for (auto simplex : complex.skeleton_simplex_range(1)) {
-    //    if (complex.dimension(simplex) == 0) continue;
-    //    cout << "\t";
-    //    for (auto v : complex.simplex_vertex_range(simplex)) {
-    //        cout << v << " ";
-    //    }
-    //    cout << ": " << complex.filtration(simplex) << "\n";
-    //}
-
-    //cout << "Number of edges in MST: " << mst.size() << "\n";
-    //cout << "MST edges:\n";
-    //for (EdgeDescriptor ed : mst)
-    //{
-    //    VertexDescriptor s = source(ed, graph);
-    //    VertexDescriptor t = target(ed, graph);
-    //    std::cout << "\t" << s << "--" << t << "\n";
-    //}
+    cout << LatticeDiameter(U) << "\n";
 }
 
 int main() 
 {
-    //testGudhiAlpha2D();
-    testGudhiAlpha3D();
+    testLatticeDiameter();
+}
+
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+#include <pybind11/numpy.h>
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(periodica, m) {
+   m.def("delaunay_skeleton", &DelaunaySkeleton, "Compute 1-skeleton of 2D & 3D Delaunay triangulations",
+         py::arg("points"));
+   m.def("euclidean_mst", &EuclideanMST, "Compute 2D & 3D Euclidean minimum spanning trees",
+         py::arg("points"));
+   m.def("lattice_diameter", &LatticeDiameter, "Compute diameter of a unit cell in the lattice, input should be a 2x2 or 3x3 matrix representing the lattice basis",
+         py::arg("U"));
 }
